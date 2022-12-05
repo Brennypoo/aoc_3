6445f66ab4668ea1d130fc98854784f0da7055e5
Brendon Earl <63430919+Brennypoo@users.noreply.github.com>
2022-12-05 22:42:48 +0000

    add readme and clean code

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ def findSimilarChars(line):
 
 
 def getSimilarCharsInGroup(group):
-    if len(group) % 3 != 0:
-        exit(1)
-    else:
-        first, second, third = group
-        for char in first:
-            if (char in second) and (char in third):
-                return char
+    first, second, third = group
+    for char in first:
+        if (char in second) and (char in third):
+            return char
     return None
 
 
@@ -21,13 +18,12 @@ def main(filename):
     arr = [line.strip() for line in open(filename).readlines()]
 
     get_priority = lambda char: ord(char) - 96 if ord(char) > 90 else ord(char) - 38
-    total_priorities = 0
-    for line in arr:
-        similar_char = findSimilarChars(line)
-        total_priorities += get_priority(similar_char)
-
     get_group = lambda num, array: [array[(i * 3)], array[(i * 3) + 1], array[(i * 3) + 2]]
-    group_badge_total = 0
+
+    total_priorities, group_badge_total = 0, 0
+
+    for line in arr:
+        total_priorities += get_priority(findSimilarChars(line))
     for i in range(int(len(arr) / 3)):
         group_badge_total += get_priority(getSimilarCharsInGroup(get_group(i, arr)))
 
